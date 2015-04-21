c66c434cb455a565114ade9feab7f6366812e28d
Nathaniel Nelson <nelson.nleroy@gmail.com>
2015-04-20 19:24:09 -0600

    All boards are now subscribed to Trellonos

--- trellonos/trellonos.py
+++ trellonos/trellonos.py
@@ -1,9 +1,4 @@
-import re
-
 from board import Board
-
-
-TRELLONOS_REGEX = re.compile('^<.+>$')
 
 
 class Trellonos(object):
@@ -16,23 +11,18 @@ class Trellonos(object):
         self.__boards = {}
         self.__closed_boards = {}
 
-        # Iterate and initialize board objects from subscribed Trello boards
+        # Iterate and initialize board objects from Trello boards
         for trello_board in trello.get_boards():
-            board_name = trello_board['name']
+            board_key = trello_board['name']
 
-            # Only pay attention to boards marked with names in <>
-            if re.search(TRELLONOS_REGEX, board_name):
-                # Strip board name of tags before mapping
-                board_key = board_name[1:-1]
+            # Construct the Trellonos board object
+            board = Board(trello, trello_board)
 
-                # Construct the Trellonos board object
-                board = Board(trello, trello_board)
-
-                # Separate open and closed boards in 2 maps
-                if board.open:
-                    self.__boards[board_key] = board
-                else:
-                    self.__closed_boards[board_key] = board
+            # Separate open and closed boards in 2 maps
+            if board.open:
+                self.__boards[board_key] = board
+            else:
+                self.__closed_boards[board_key] = board
 
     @property
     def boards(self):
