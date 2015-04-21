27e16489f3c72195e1bf10f25eeec40f0338d235
Nathaniel Nelson <nelson.nleroy@gmail.com>
2015-04-20 19:30:00 -0600

    Revert "All boards are now subscribed to Trellonos"
    This reverts commit d1c1a7533d9b23b77db167fe99c18d0942815d49.

--- trellonos/trellonos.py
+++ trellonos/trellonos.py
@@ -1,4 +1,9 @@
+import re
+
 from board import Board
+
+
+TRELLONOS_REGEX = re.compile('^<.+>$')
 
 
 class Trellonos(object):
@@ -11,18 +16,23 @@ class Trellonos(object):
         self.__boards = {}
         self.__closed_boards = {}
 
-        # Iterate and initialize board objects from Trello boards
+        # Iterate and initialize board objects from subscribed Trello boards
         for trello_board in trello.get_boards():
-            board_key = trello_board['name']
+            board_name = trello_board['name']
 
-            # Construct the Trellonos board object
-            board = Board(trello, trello_board)
+            # Only pay attention to boards marked with names in <>
+            if re.search(TRELLONOS_REGEX, board_name):
+                # Strip board name of tags before mapping
+                board_key = board_name[1:-1]
 
-            # Separate open and closed boards in 2 maps
-            if board.open:
-                self.__boards[board_key] = board
-            else:
-                self.__closed_boards[board_key] = board
+                # Construct the Trellonos board object
+                board = Board(trello, trello_board)
+
+                # Separate open and closed boards in 2 maps
+                if board.open:
+                    self.__boards[board_key] = board
+                else:
+                    self.__closed_boards[board_key] = board
 
     @property
     def boards(self):
