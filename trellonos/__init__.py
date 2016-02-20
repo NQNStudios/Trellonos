ec520a9c25ac9b1d2f647a3cd5f48c6979f7bb7e
Nathaniel Nelson <nelson.nleroy@gmail.com>
2016-02-20 09:27:10 -0700

    Trellonos can now be installed system-wide

---
+++ trellonos/__init__.py
@@ -0,0 +1,10 @@
+""" Package for automation of Trello operations """
+
+# Internal files.
+from .board import *
+from .card import *
+from .githubtools import *
+from .list import *
+from .logtools import *
+from .trellonos import *
+from .trellotools import *
